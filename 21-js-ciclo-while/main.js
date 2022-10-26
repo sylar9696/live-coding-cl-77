504b512489977f07f5fa2a1d23ffa33753f2fbec
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-10-26 13:16:29 +0200

    ciclo while

---
+++ 21-js-ciclo-while/main.js
@@ -0,0 +1,67 @@
+/*
+- creare un array con alimenti da comprare al supermercato
+*/
+
+const lista = [
+  'latte',
+  'uova',
+  'pomodori',
+  'pizza'
+];
+
+const listaSpesa = document.getElementById('listaSpesa');
+
+// //versione con il for
+// for( let i=0; i < lista.length; i++ ){
+//   //salavre in una variabile il nuovo elemento html da creare
+//   const li = document.createElement('li');
+//   //scrivere dentro il tag <li></li>
+//   li.innerText = lista[i];
+//   //inseriamo <li> dentro <ul></ul>
+//   listaSpesa.append(li);
+// }
+
+//versione con while
+
+let i = 0;
+
+while( i < lista.length ){
+
+  //mio codice
+  const li = document.createElement('li');
+  //inserire una classe a <li></li>
+  li.innerText = lista[i];
+  li.classList.add('text-center', 'bg-blue');
+  listaSpesa.append(li);
+
+  i++;
+}
+
+/*
+@here
+Ciao Ragazzi,
+Esercizio di oggi:
+nome repo: `js-jsnacks-blocco-3`
+
+
+Eseguite questi programmi con il ciclo while
+
+*Snack1*
+Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+*Snack2*
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*Snack3* (Bonus)
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array,
+ognuno formato da 10 numeri casuali da 1 a 100.
+Ogni volta che ne crei uno, stampalo.
+
+*Snack4 (Bonus)*
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+Interrompi il ciclo appena il nome è stato trovato.
+
+*/
