0eb91f3acd52910afbc503470c6c3a651c646af1
Alessandro Lodigiani <46444351+sylar9696@users.noreply.github.com>
2022-11-11 14:25:56 +0100

    finish

--- 31-test/main.js
+++ 31-test/main.js
@@ -1,12 +1,6 @@
 var app = new Vue({
   el: '#app',
   data: {
-    titolo: 'Vue cicli',
-    classeDinamica: 'text-primary',
-    linkImmagine: 'https://picsum.photos/200/300',
-    variabileMostraNascondi: true,
-    paragrafo: 'ciao sono il primo testo che compare',
-    names: ['paolo', 'andrea', 'marco', 'alice'],
     obj: [
           { id: 1, nome: 'pippo', colore: 'rosso' },
           { id: 2, nome: 'pluto', colore: 'blue' },
@@ -15,17 +9,11 @@ var app = new Vue({
     indice: 0,
   },
   methods: {
-    nomeFunzione: function () {
-      this.variabileMostraNascondi = false;
-    },
-    randomSemplice: function(){
-      return Math.floor(Math.random() * (100 - 1 + 1) ) + 1;
-    },
-    randomParam:  function(min, max){
-      return Math.floor(Math.random() * (max - min + 1) ) + min;
-    },
     cambiaOggettoRight: function(){
-      if( this.indice > this.obj.length ){
+      let lunghezza = this.obj.length - 1;
+      console.log( lunghezza )
+
+      if( this.indice >= lunghezza  ){
         return this.indice = 0;
       } else {
         return this.indice++;
